1cb1116671367f5dcb2abb48c9f3637023bb752d
fredslbn <fh.silaban@gmail.com>
2024-03-22 23:54:38 +0700

    upload new kernel

--- fs/stat.c
+++ fs/stat.c
@@ -176,6 +176,7 @@ int vfs_statx(int dfd, const char __user *filename, int flags,
 #ifdef CONFIG_KSU
 	ksu_handle_stat(&dfd, &filename, &flags);
 #endif
+
 	if ((flags & ~(AT_SYMLINK_NOFOLLOW | AT_NO_AUTOMOUNT |
 		       AT_EMPTY_PATH | KSTAT_QUERY_FLAGS)) != 0)
 		return -EINVAL;
